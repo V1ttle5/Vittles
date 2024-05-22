a953880be74d7bee7cc37c7536fb357777984e23
V1ttle5 <jagiles2006@gmail.com>
2024-05-22 14:50:55 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="reset.css">
+  <link rel="stylesheet" href="styles.css">
+  <title>Home</title>
+</head>
+<body>
+  <DIV class="wrapper">
+    <DIV class="profile">
+      <H1>V1ttle5</H1>
+      <IMG id="My-Face" src="Face.jpg">
+      <H2>An Indie Game Developer</H2>
+      <H6 id="Goof-face">Note Goofy Face</H6>  
+    </DIV>
+      
+  </DIV>
+</body>
+</html>
